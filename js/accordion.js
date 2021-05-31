6a1b70f98818f915eb6ac4a6ed655655196482e4
Oksana Repchak <oksa.repchak@gmail.com>
2021-05-31 14:57:40 +0300

    added slider team

--- js/accordion.js
+++ js/accordion.js
@@ -2,7 +2,7 @@ let acc = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
+    this.classList.toggle("active-accordion");
     const panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
